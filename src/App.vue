29eb02674a91a455aa2f4ab29d447b764cb78f65
Thomas Veit <thomas.veit93@gmail.com>
2021-09-03 19:19:43 +0100

    navbar component created

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Navbar />
   <router-view />
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap");
---
+++ src/components/Navbar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div className="navbar">
+    <h1>Poké Antiques</h1>
+    <ul>
+      <router-link to="/">
+        <li>Home</li>
+      </router-link>
+      <router-link to="/shop">
+        <li>Shop</li>
+      </router-link>
+      <router-link to="/cart">
+        <li>Cart <span>(0)</span></li>
+      </router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style lang="scss">
+.navbar {
+  display: flex;
+  align-items: center;
+  height: 8vh;
+  min-height: 50px;
+  width: 100%;
+  box-shadow: 0 2px 8px #e0e0e0;
+  background-color: white;
+  padding: 0 3.9rem;
+  box-sizing: border-box;
+  z-index: 3;
+
+  h1 {
+    font-family: walkway;
+    font-size: 2.2rem;
+    width: 40%;
+  }
+
+  span {
+    font-size: 0.9rem;
+    color: #333;
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 4rem;
+    list-style: none;
+    width: 57%;
+
+    li {
+      font-weight: 300;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: #000;
+  }
+}
+</style>
