66e9fe525989b3bf3c55fe3dd3608133ec7967f6
Thomas Veit <thomas.veit93@gmail.com>
2021-09-06 10:23:58 +0100

    Loading component created

---
+++ src/components/Loading.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="loading-container">
+    <p>Loading ...</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.loading-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 92vh;
+  font-family: walkway;
+
+  p {
+    font-size: 2rem;
+  }
+}
+</style>
